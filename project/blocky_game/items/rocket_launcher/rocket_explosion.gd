0cf01b53e0cf1ce165a45212903a160678111598
Marc Gilleron <marc.gilleron@gmail.com>
2020-08-17 21:27:00 +0100

    Added light to explosions

--- project/blocky_game/items/rocket_launcher/rocket_explosion.gd
+++ project/blocky_game/items/rocket_launcher/rocket_explosion.gd
@@ -1,10 +1,12 @@
 extends Spatial
 
 onready var _particles = $Particles
+onready var _animation_player = $AnimationPlayer
 
 
 func _ready():
 	_particles.emitting = true
+	_animation_player.play("explode")
 
 
 func _on_Timer_timeout():
