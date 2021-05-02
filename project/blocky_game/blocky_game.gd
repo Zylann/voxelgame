aa87dabbf16780c923c59e3dadd078d912e458c8
Marc Gilleron <marc.gilleron@gmail.com>
2021-05-02 18:10:16 +0100

    Save game when the window is closed

--- project/blocky_game/blocky_game.gd
+++ project/blocky_game/blocky_game.gd
@@ -1,6 +1,7 @@
 extends Node
 
 onready var _light = $DirectionalLight
+onready var _terrain = $VoxelTerrain
 
 
 func _unhandled_input(event):
@@ -10,4 +11,15 @@ func _unhandled_input(event):
 			if event.scancode == KEY_L:
 				# Toggle shadows
 				_light.shadow_enabled = not _light.shadow_enabled
+
+
+func _notification(what: int):
+	match what:
+		NOTIFICATION_WM_QUIT_REQUEST:
+			# Save game when the user closes the window
+			_save_world()
+
+
+func _save_world():
+	_terrain.save_modified_blocks()
 
